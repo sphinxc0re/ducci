1b7851a1f525e362bb7dc3a8b6d15049b86d3dd3
Julian Laubstein <contact@julianlaubstein.de>
2017-04-26 17:26:22 +0200

    Added license

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,24 @@
+/*
+    Ducci - A library for creating Ducci sequences from n-tuples
+    Copyright (C) 2017  Julian Laubstein
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 //! A library for creating Ducci sequences from n-tuples
 
-type Tuple = Vec<i64>;
+pub type Tuple = Vec<i64>;
 
 pub fn create_ducci_sequence(entry_sequence: Tuple) -> Vec<Tuple> {
     if !is_power_of_two(entry_sequence.len()) {
